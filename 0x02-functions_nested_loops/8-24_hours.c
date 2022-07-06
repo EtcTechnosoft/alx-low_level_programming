de259d3e137db1e5c0961841f692aa14f2258261
Israel A. Oteniola <79619296+EtcTechnosoft@users.noreply.github.com>
2022-07-06 21:59:29 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,48 +1,45 @@
 #include "main.h"
 /**
-* jack_bauer - every minutes of the day is printed
-*
+* jack_bauer - prints every minute of the day for Jack Bauer
+* 
 * Return:void
 */
 void jack_bauer(void)
 {
-	int h1;
+int h1;
+int h2;
+int m1;
+int m2;
+int x = 9;
+h2 = 0;
 
-	int h2;
-
-	int m1;
-
-	int x = 9;
-
-	h2 = 0;
-
-	while (h2 <= 2)
+while (h2 <= 2)
+{
+	if (h2 == 2)
 	{
-		if (h2 == 2)
-		{
-			x = 3;
-		}
-		h1 = 0;
-		while (h1 <= x)
-		{
-			m2 = 0;
-			while (m2 <= 5)
-			{
-				m1 = 0;
-				while (m1 <= 9)
-				{
-					_putchar('0' + h2);
-					_putchar('0' + h2);
-					_putchar(':');
-					_putchar('0' + m2);
-					_putchar('0' + m1);
-					_putchar('\n');
-					m1++;
-				}
-				m2++;
-			}
-			h1++;
-		}
-		h2++;
+		x = 3;
 	}
+	h1 = 0;
+	while (h1 <= x)
+	{
+		m2 = 0;
+		while (m2 <= 5)
+		{
+			m1 = 0;
+			while (m1 <= 9)
+			{
+				_putchar('0' + h2);
+				_putchar('0' + h1);
+				_putchar(':');
+				_putchar('0' + m2);
+				_putchar('0' + m1);
+				_putchar('\n');
+				m1++;
+			}
+			m2++;
+		}
+		h1++;
+	}
+	h2++;
+}
 }
