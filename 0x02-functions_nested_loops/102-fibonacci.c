a526e1dcadee911a96edc59fb94ed49eefff6a3e
Israel A. Oteniola <79619296+EtcTechnosoft@users.noreply.github.com>
2022-07-06 22:38:40 +0100

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - code block
+ * Description: prints sum of even-valued  number < 4,000,000
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0, y = 1, num = 0;
+	int sum = 0;
+
+	while (num < 4000000)
+	{
+		num = x + y;
+		x = y;
+		y = num;
+		if (num % 2 == 0)
+      sum += num;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
