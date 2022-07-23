bda2e28f6342449bc65469914fbbc735b9f1df9d
Israel Oteniola <success.ote86@gmail.com>
2022-07-22 18:44:10 -0700

    This object

--- 0x09-static_libraries/11-stat.c
+++ 0x09-static_libraries/11-stat.c
@@ -1,24 +1,8 @@
+
 #include "main.h"
 
-/**
- * *_strcat - Concatenates two pointers
- *
  *@src : Has the string to be concatenated
  @dest: The pointer to hold the concatenation
- *
  *Return: returns the concatenated
- */
-char *_strcat(char *dest, char *src)
-{
-	int a;
-	int b;
-
-	for (a = 0, dest[a] a != 0, a++)
-	{
-	}
-	for (b = 0, src[b] b != 0, b++)
-	{
-		dest[a] = src[b];
-	}
 	return (dest);
 }
