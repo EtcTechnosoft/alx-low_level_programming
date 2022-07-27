bcf09bfe5c5a6e31eb618faf197f87dfb6e26ac2
Israel Oteniola <success.ote86@gmail.com>
2022-07-27 15:53:14 +0100

    Library files

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - check for lowercase alphabets
+* @c: The character for the case
+*
+* Description: Checks for lowercase  character
+*
+* Return:1(success) or 0(otherwise)
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _abs - absolute value
+* @x: numbers to check
+*
+* Return:0 or 1
+*/
+int _abs(int x)
+{
+	return (x >= 0 ? x : x * -1);
+}
