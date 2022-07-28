d14400adc0c20ffe060a1768449b098358b298a0
Israel Oteniola <success.ote86@gmail.com>
2022-07-28 19:52:34 +0100

    O-strcat

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -13,10 +13,10 @@ char *_strcat(char *dest, char *src)
 	int a;
 	int b;
 
-	for (a = 0, dest[a] a != 0, a++)
+	for (a = 0, dest[a]; a != 0, a++)
 	{
 	}
-	for (b = 0, src[b] b != 0, b++)
+	for (b = 0, src[b]; b != 0, b++)
 	{
 		dest[a] = src[b];
 	}
